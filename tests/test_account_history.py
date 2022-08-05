d34c4ef8cdce15602d7e55fdb67e22a1f67208ac
stevenmchaves <stevenmchaves@gmail.com>
2022-08-04 23:31:15 -0400

    fix names of methods

--- tests/test_account_history.py
+++ tests/test_account_history.py
@@ -12,17 +12,17 @@ def test_invalid_account_history():
     with pytest.raises(TypeError):
         AccountHistory({})
     
-def test_user_account():
+def test_account_history():
     assert account_history.amount == 5
     assert account_history.balance == 500
     assert account_history.date_time == now_datetime
 
-def test_user_account_negative():
+def test_account_history_negative():
     assert account_history_negative.amount == 50
     assert account_history_negative.balance == -500
     assert account_history_negative.date_time == now_datetime
 
-def test_user_account_print(capsys):
+def test_account_history_print(capsys):
     print(account_history)
     expected_out = now_datetime.strftime("%Y-%m-%d %H:%M:%S")  + ' $5.00 $500.00\n'
     assert expected_out in capsys.readouterr()
